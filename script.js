8f2e68fe7636b883e437c76822015c92ad36a437
yetsunu <ljdeguzman0010val@student.fatima.edu.ph>
2025-03-09 20:13:46 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ function displayCat() {
     };
 }
 
-// Function to display the kuromi.gif
+// Function to display the cat-heart.gif
 function displayKuromi() {
     // Clear existing content in the image container
     document.getElementById('image-container').innerHTML = '';
